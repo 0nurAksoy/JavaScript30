000527aef0ce853ae3d9e38598ae44d32c3041a9
Onur <onur.aksoy.3445@gmail.com>
2026-03-27 18:26:00 +0300

    All done

--- DrumKit/script.js
+++ DrumKit/script.js
@@ -1,9 +1,19 @@
-let pressingKey = '';
-let playAudio = '';
+function removeTransition(e) {
+    if(e.propertyName !== 'transform') return;
+    e.target.classList.remove('playing');
+};
 
-window.addEventListener('keydown', e => {
-    pressingKey = document.querySelector(`[data-key="${e.keyCode}"]`);
-    playAudio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+function playSound (e) {
+    const pressingKey = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    const playAudio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     if(!playAudio) return;
+    
+    pressingKey.classList.add('playing');
+    playAudio.currentTime = 0;
     playAudio.play();
-});
+};
+
+const keys = Array.from(document.querySelectorAll('.key'));
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+window.addEventListener('keydown', playSound);
